4db68c2a56fe45df7e289d88271c7988c5be63de
Severn-J-Lortie <severn.lortie@gmail.com>
2025-02-22 12:55:11 -0500

    feat(qs): use standard file for testing and modify bash script to use mpirun for CAC compat

--- trace-qs.sh
+++ trace-qs.sh
@@ -5,8 +5,9 @@ QS_DIR=~/git/Quicksilver # You'll probably need to change this
 UF_DIR=~/git/uftrace-2-python # This as well
 
 
-sudo echo # doing a no-op to force sudo before the turbostat script
-uftrace record -t 5ms -- ${QS_DIR}/src/qs --nx 10 --ny 10 --nz 10 --nSteps 5 --inputFile ${QS_DIR}/Examples/NoFission/noFission.inp &
+#sudo echo # doing a no-op to force sudo before the turbostat script
+
+mpirun -- uftrace record -t 5ms -- ${QS_DIR}/src/qs --inputFile ${UF_DIR}/qs-test-cfg.inp &
 qs_pid=$!
 python ${UF_DIR}/turbostat.py &
 turbo_pid=$!
